f503d88a3b957d9b3cf5482732e29d8db18c5958
NedimHodzic04 <nedim.hodzic@stu.ibu.edu.ba>
2024-11-24 13:10:06 +0100

    commit

---
+++ reviews.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+const reviewCard1 = document.querySelector('#review-card1');
+const reviewCard2 = document.querySelector('#review-card2');
+const reviewCard3 = document.querySelector('#review-card3');
+
+reviewCard1.addEventListener('click', () => {
+    window.location.href = 'https://maps.app.goo.gl/GzQnUoRzBFwfpeZk8';
+})
+
+
+reviewCard2.addEventListener('click', () => {
+    window.location.href = 'https://maps.app.goo.gl/JDDFTaFNmAgF7uau7';
+})
+
+reviewCard3.addEventListener('click', () => {
+    window.location.href = 'https://maps.app.goo.gl/hpBQzuirSVRuiS3f7';
+})
+
+});
+
